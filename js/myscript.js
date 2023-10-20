6e6aed8e480bdde58ee5db2cdc71f45ae483dc08
LuvasTheGloves <142498451+LuvasTheGloves@users.noreply.github.com>
2023-10-20 17:57:07 +0000

    javascript not ez ngl

--- js/myscript.js
+++ js/myscript.js
@@ -18,3 +18,63 @@ function trocarClasse() {
     var fraseatual = document.getElementById("frase");
     fraseatual.className = "h1_2";
 }
+
+function trocarTexto() {
+    var elemento = document.getElementById("frase")
+    var elementoNovo = document.getElementById("novaFrase").value
+    elemento.innerHTML = elementoNovo
+}
+
+function somar() {
+    let campoN1 = document.getElementById("n1")
+    let valorN1 = parseInt(campoN1.value)
+
+    let campoN2 = document.getElementById("n2")
+    let valorN2 = parseInt(campoN2.value)
+
+    let resultadoDaSoma = valorN1 + valorN2
+    
+    let campoResultado = document.getElementById("resultado")
+    campoResultado.Value = resultadoDaSoma
+}
+
+function subtrair() {
+    let campoN1 = document.getElementById("n1")
+    let valorN1 = parseInt(campoN1.value)
+
+    let campoN2 = document.getElementById("n2")
+    let valorN2 = parseInt(campoN2.value)
+
+    let resultadoDaSubtraçao = valorN1 - valorN2
+    
+    let campoResultado = document.getElementById("resultado")
+    campoResultado.Value = resultadoDaSoma
+}
+
+function multiplicar() {
+    let campoN1 = document.getElementById("n1")
+    let valorN1 = parseInt(campoN1.value)
+
+    let campoN2 = document.getElementById("n2")
+    let valorN2 = parseInt(campoN2.value)
+
+    let resultadoMultplicaçao = valorN1 * valorN2
+    
+    let campoResultado = document.getElementById("resultado")
+    campoResultado.Value = resultadoDaSoma
+}
+
+function dividir() {
+    let campoN1 = document.getElementById("n1")
+    let valorN1 = parseInt(campoN1.value)
+
+    let campoN2 = document.getElementById("n2")
+    let valorN2 = parseInt(campoN2.value)
+
+    let resultadoDaDivisao = valorN1 / valorN2
+    
+    let campoResultado = document.getElementById("resultado")
+    campoResultado.Value = resultadoDaSoma
+}
+
+
